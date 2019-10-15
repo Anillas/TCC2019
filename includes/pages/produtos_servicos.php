cd4117e9492cecec401b7458b5c1c22ea2b86a8f
Anillas <anillas@mail.com>
2019-10-15 09:52:17 -0300

    Adicionando arquivos

---
+++ includes/pages/produtos_servicos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "../complements/meta.php"; ?>
+	<?php include "../complements/link.php"; ?>
+	<title>Produtos e Serviços</title>
+</head>
+<body>
+	<?php include "../complements/menu.php"; ?>
+	<?php include "../complements/footer.php"; ?>
+	<?php include "../complements/script.php"; ?>
+</body>
+</html>
---
+++ includes/pages/sobre.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "../complements/meta.php"; ?>
+	<?php include "../complements/link.php"; ?>
+	<title>Sobre</title>
+</head>
+<body>
+	<?php include "../complements/menu.php"; ?>
+	<?php include "../complements/footer.php"; ?>
+	<?php include "../complements/script.php"; ?>
+</body>
+</html>
