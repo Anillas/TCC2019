a0bfa7132e3742eef85a474fed26b09485a38e8e
Anillas <anillas@mail.com>
2019-11-08 02:04:10 -0200

    Ajeitando poucas coisas

--- controle/conexao.php
+++ controle/conexao.php
@@ -1,8 +1,8 @@
 <?php
    	$db_host = "localhost";
-	$db_nome = "banco";
-	$db_usuario = "root";
-	$db_senha = "";
+	$db_nome = "banco"; //id11299677_banco_tcc
+	$db_usuario = "root"; //id11299677_sani
+	$db_senha = ""; //senha12345
     $db_driver = "mysql";
     $connect = mysqli_connect($db_host,$db_usuario,$db_senha,$db_nome);
 ?>
